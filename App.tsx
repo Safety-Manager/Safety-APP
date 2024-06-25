3785cc889fb52ad38bb79e3c3576119a82a92c32
sanguu516 <sanguu516@naver.com>
2024-06-25 19:06:58 +0900

    chore: Update Node.js version to v18.19.1 and add bundle dependency and update image sources

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreens from '@screens/MainScreens';
 import WelcomeScreens from '@screens/WelcomeScreens';
@@ -8,6 +8,13 @@ import SearchInfoScreens from '@screens/SearchInfoScreens';
 import SearchScreens from '@screens/SearchScreens';
 import MyTabs from '@navigation/MyTabs';
 import CodePush from 'react-native-code-push';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  COOKIE_ACCESS_TOKEN,
+  COOKIE_REFRESH_TOKEN,
+  API_URL,
+} from './src/config/constants';
+import {NavigationProp, ParamListBase} from '@react-navigation/native';
 
 // 타입 정의
 export type RootStackParamList = {
@@ -30,49 +37,69 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen
-            name="Main"
-            component={MainScreens}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Welcome"
-            component={WelcomeScreens}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Home"
-            component={MyTabs}
-            options={{headerShown: false, gestureEnabled: false, title: ''}}
-          />
-          <Stack.Screen
-            name="Search"
-            component={SearchScreens}
-            options={{headerShown: false, gestureEnabled: false, title: ''}}
-          />
-          <Stack.Screen
-            name="SearchInfo"
-            component={SearchInfoScreens}
-            options={{
-              headerShown: false,
-              headerTitle: '',
-            }}
-          />
-        </Stack.Navigator>
+        <RootNavigator />
       </NavigationContainer>
     </QueryClientProvider>
   );
 }
 
+const RootNavigator = () => {
+  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
+  useEffect(() => {
+    const checkAccessToken = async () => {
+      const accessToken = await AsyncStorage.getItem(COOKIE_ACCESS_TOKEN);
+
+      if (accessToken) {
+        navigation.navigate('Home');
+      }
+    };
+
+    checkAccessToken();
+  }, [navigation]);
+
+  return (
+    <Stack.Navigator initialRouteName="Main">
+      <Stack.Screen
+        name="Main"
+        component={MainScreens}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreens}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={MyTabs}
+        options={{headerShown: false, gestureEnabled: false, title: ''}}
+      />
+      <Stack.Screen
+        name="Search"
+        component={SearchScreens}
+        options={{headerShown: false, gestureEnabled: false, title: ''}}
+      />
+      <Stack.Screen
+        name="SearchInfo"
+        component={SearchInfoScreens}
+        options={{
+          headerShown: false,
+          headerTitle: '',
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const codePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_START,
-  updateDialog: {
-    title: '...',
-    optionalUpdateMessage: '...',
-    optionalInstallButtonLabel: '업데이트',
-    optionalIgnoreButtonLabel: '아니요.',
-  },
+  // updateDialog: {
+  //   title: '...',
+  //   optionalUpdateMessage: '...',
+  //   optionalInstallButtonLabel: '업데이트',
+  //   optionalIgnoreButtonLabel: '아니요.',
+  // },
   installMode: CodePush.InstallMode.IMMEDIATE,
 };
 
